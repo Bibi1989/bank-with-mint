f883414136281e20ba5e0bd61dd8315aa1ab2082
bibi1989 <bibiaremieye@gmail.com>
2020-07-12 17:01:29 +0100

    built the the chart component

--- src/components/Main/Style.js
+++ src/components/Main/Style.js
@@ -1,1 +1,71 @@
 import styled from "styled-components";
+
+export const Row = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 689px 379px;
+  gap: 5px;
+
+  border: 1px solid #eef8fd;
+  box-sizing: border-box;
+  border-radius: 2px;
+  margin-top: 27px;
+`;
+export const GridOne = styled.div`
+  background: #ffffff;
+`;
+export const GridTwo = styled.div`
+  background: #ffffff;
+`;
+export const DivOne = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 26px 30px 0px 34px;
+  padding-bottom: 45px;
+`;
+export const DivTwo = styled.div``;
+export const H4 = styled.h4``;
+export const Select = styled.div`
+  border: 1px solid #dde0e3;
+  border-radius: 4.25px;
+  padding: 10px 11px;
+  font-family: Segoe UI;
+  font-size: 12px;
+  line-height: 16px;
+  display: flex;
+  align-items: flex-end;
+
+  color: #7f8fa4;
+`;
+export const Days = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 32px;
+
+  h4 {
+    font-family: Segoe UI;
+    font-size: 14px;
+    line-height: 19px;
+    color: #000000;
+  }
+`;
+export const Shape = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const ShapeDiv = styled.div``;
+export const ArrowButtons = styled.div`
+  padding-left: 39.5px;
+
+  img:first-child {
+    display: inline-block;
+    padding-right: 13px;
+  }
+`;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  Row,
+  Grid,
+  GridOne,
+  GridTwo,
+  DivOne,
+  DivTwo,
+  Select,
+  Days,
+  H4,
+  Shape,
+  ArrowButtons,
+} from "./Style";
 
 import Card from "./Cards";
 import { cardsDetails } from "../../utils/data";
@@ -26,6 +39,10 @@ const Main = () => {
                 <span>1 Jan - 1 Jun</span>
                 <img src='./assets/arrow_down.svg' alt='arrow down' />
               </Select>
+              <ArrowButtons>
+                <img src='./assets/left_button.svg' alt='left button' />
+                <img src='./assets/right_button.svg' alt='right button' />
+              </ArrowButtons>
             </Shape>
           </DivOne>
           <Days>
@@ -37,7 +54,30 @@ const Main = () => {
             <img src='./assets/big_chart.svg' alt='big chart' />
           </DivTwo>
         </GridOne>
-        <GridTwo></GridTwo>
+
+        {/* second grid */}
+        <GridTwo>
+          <Order>
+            <H3>Orders</H3>
+            <Progress>
+              <ProgressBar></ProgressBar>
+            </Progress>
+            <List>
+              <P>
+                <SpanTitle>Pending Orders:</SpanTitle>
+                <SpanAmount>20</SpanAmount>
+              </P>
+              <P>
+                <SpanTitle>Reconcilled Orders:</SpanTitle>
+                <SpanAmount>20</SpanAmount>
+              </P>
+              <P>
+                <SpanTitle>Total Orders:</SpanTitle>
+                <SpanAmount>20</SpanAmount>
+              </P>
+            </List>
+          </Order>
+        </GridTwo>
       </Grid>
     </div>
   );
@@ -45,58 +85,32 @@ const Main = () => {
 
 export default Main;
 
-export const Row = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 16px;
+export const Order = styled.div`
+  padding: 21px 25px;
 `;
-
-export const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 689px 379px;
-  background: #ffffff;
-  border: 1px solid #eef8fd;
-  box-sizing: border-box;
-  border-radius: 2px;
-  margin-top: 27px;
-`;
-export const GridOne = styled.div``;
-export const GridTwo = styled.div`
-  background: red;
-`;
-export const DivOne = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 26px 30px 0px 34px;
-  padding-bottom: 45px;
-`;
-export const DivTwo = styled.div``;
-export const H4 = styled.h4``;
-export const Select = styled.div`
-  border: 1px solid #dde0e3;
-  border-radius: 4.25px;
-  padding: 10px 11px;
+export const H3 = styled.h3`
   font-family: Segoe UI;
-  font-size: 12px;
+  font-size: 14px;
   line-height: 16px;
-  display: flex;
-  align-items: flex-end;
-
-  color: #7f8fa4;
+  color: #262626;
+  padding-bottom: 11px;
 `;
-export const Days = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 30px;
-
-  h4 {
-    font-family: Segoe UI;
-    font-size: 14px;
-    line-height: 19px;
-    color: #000000;
-  }
+export const Progress = styled.div`
+  background: #fdc203;
+  height: 4px;
+  width: calc(242px + 63px);
+  border-radius: 5px;
+  overflow: hidden;
+  margin-bottom: 11px;
 `;
-export const Shape = styled.div``;
-export const ShapeDiv = styled.div``;
+export const ProgressBar = styled.div`
+  background: #27ae60;
+  width: 242px;
+  height: 100%;
+`;
+export const List = styled.div``;
+export const P = styled.p`
+  display: flex;
+`;
+export const SpanTitle = styled.div``;
+export const SpanAmount = styled.div``;
